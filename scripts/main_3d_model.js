141f65df13cd22510f6de75181d8ec6e8dd42f97
Taras Cha <smth>
2016-03-02 15:28:27 +0200

    ARBase;

--- scripts/main_3d_model.js
+++ scripts/main_3d_model.js
@@ -1,242 +1,206 @@
-var main3dModel = function () {
-    'use strict';
+var main3dModel = null;
 
-    var video, canvas, context, imageData, detector, posit,
-        renderer, scene, camera, animated_mesh,
-        step = 0.0,
-        animation,
-        clock = new THREE.Clock(),
-        modelSize = 0.12;//35.0; //millimeters
+ARBase.prototype.onInit = function () {
+    this.clock = new THREE.Clock(),
+    this.clock.autoStart = true;
 
-    clock.autoStart = true;
+    this.detector = new AR.Detector();
+    this.detector.detect = function (image) {
+        var qr_data;
 
-    function animate(skinnedMesh) {
-        var materials = skinnedMesh.material.materials,
-            k;
+        CV.grayscale(image, this.grey);
+        CV.adaptiveThreshold(this.grey, this.thres, 2, 7);
 
-        for (k in materials) {
-            materials[k].skinning = true;
-        }
-        animation = new THREE.Animation(skinnedMesh, skinnedMesh.geometry.animation);
-        animation.play();
-    }
+        this.contours = CV.findContours(this.thres, this.binary);
 
-    function createRendererAndScene() {
-        scene = new THREE.Scene();
-        //camera = new THREE.PerspectiveCamera( 75, video.width/video.height, 0.1, 1000 );
-        camera = new THREE.PerspectiveCamera(40, canvas.width / canvas.height, 1, 1000);
+        this.candidates = this.findCandidates(this.contours, image.width * 0.20, 0.05, 10);
+        this.candidates = this.clockwiseCorners(this.candidates);
+        this.candidates = this.notTooNear(this.candidates, 5);
 
-        renderer = new THREE.WebGLRenderer({
-            antialias	: true,
-            alpha		: true
-        });
-        renderer.setSize(video.width, video.height);
-        document.body.appendChild(renderer.domElement);
-
-        var geometry = new THREE.BoxGeometry(1, 1, 1),
-            material = new THREE.MeshLambertMaterial({color: 0x009900}),
-            loader,
-            light2;
-        //var material = new THREE.MeshLambertMaterial( { map: THREE.ImageUtils.loadTexture('assets/Raptor/raptor.jpg')} );
-
-        THREE.CrossDomainJSONLoader = function (showStatus) {
-            THREE.JSONLoader.call(this, showStatus);
-        };
-            
-        loader = new THREE.JSONLoader();
-        //animated_mesh;
-
-        loader.load('resources/models/figure/knight.js', function (geometry, materials) {
-            var standard = new THREE.MeshLambertMaterial({color: 0x9999ff}),
-                mats,
-                mesh,
-                scale;
-
-            geometry.computeFaceNormals();
-            geometry.computeVertexNormals();
-
-            mats = new THREE.MeshFaceMaterial(materials);
-
-            //mesh = new THREE.Mesh( geometry, mats);
-            mesh = new THREE.SkinnedMesh(geometry, mats);
-
-            mesh.position.x = 0;
-            mesh.position.y = 0;
-            mesh.position.z = 0;
-
-            scale = 0.03;
-
-            mesh.scale.x = scale;
-            mesh.scale.y = scale;
-            mesh.scale.z = scale;
-            animated_mesh = mesh;
-
-            animated_mesh.visible = false;
-
-            console.log(animated_mesh);
-
-            scene.add(animated_mesh);
-            animate(animated_mesh);
-        });
-
-        camera.position.z = 5;
-
-        light2 = new THREE.DirectionalLight(0xffffff, 1, 100000);
-        light2.position.set(0, 0, 1);
-        scene.add(light2);
-    }
-    
-    function snapshot() {
-        context.drawImage(video, 0, 0, canvas.width, canvas.height);
-        imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-    }
-
-    function drawCorners(markers) {
-        var corners, corner, i, j;
-
-        context.lineWidth = 3;
-
-        for (i = 0; i < markers.length; i += 1) {
-            corners = markers[i].corners;
-
-            context.strokeStyle = "red";
-            context.beginPath();
-
-            for (j = 0; j < corners.length; j += 1) {
-                corner = corners[j];
-                context.moveTo(corner.x, corner.y);
-                corner = corners[(j + 1) % corners.length];
-                context.lineTo(corner.x, corner.y);
-            }
-
-            context.stroke();
-            context.closePath();
-
-            context.strokeStyle = "green";
-            context.strokeRect(corners[0].x - 2, corners[0].y - 2, 4, 4);
-        }
-    }
-
-    function updateObject(object, rotation, translation) {
-        object.scale.x = modelSize;
-        object.scale.y = modelSize;
-        object.scale.z = modelSize;
-
-        object.rotation.x = -Math.asin(-rotation[1][2]);
-        object.rotation.y = -Math.atan2(rotation[0][2], rotation[2][2]);
-        object.rotation.z =  Math.atan2(rotation[1][0], rotation[1][1]);
-
-        //object.rotation.x = -Math.atan2(rotation[0][2], rotation[2][2]);
-        //object.rotation.y = -Math.asin(-rotation[1][2]);
-        //object.rotation.z =  Math.atan2(rotation[1][0], rotation[1][1]);
-
-        object.position.x = translation[0];
-        object.position.y = translation[1];
-        object.position.z = -translation[2];
-    }
-    
-    function updateScene(markers) {
-        var corners, corner, pose, i;
-
-        if (markers.length > 0) {
-            animated_mesh.visible = true;
-            corners = markers[0].corners;
-
-            for (i = 0; i < corners.length; i += 1) {
-                corner = corners[i];
-                corner.x = corner.x - (canvas.width / 2);
-                corner.y = (canvas.height / 2) - corner.y;
-            }
-
-            pose = posit.pose(corners);
-
-            updateObject(animated_mesh, pose.bestRotation, pose.bestTranslation);
-
-            step += 0.025;
+        qr_data = find_QR(this.candidates);
+        if (qr_data) {
+            return [new AR.Marker(2, qr_data)];
         } else {
-            if (animated_mesh) {
-                animated_mesh.visible = false;
-            }
+            return [];
         }
-    }
+    };
 
-    function render() {
-        var delta = 0.75 * clock.getDelta();
-        THREE.AnimationHandler.update(delta);
-        renderer.render(scene, camera);
-    }
+    this.targetSize = 35.0;
+    this.positEstimator = new POS.Posit(this.targetSize, this.canvas.width);
 
-    function tick() {
-        window.requestAnimationFrame(tick);
-		
-        if (video.readyState === video.HAVE_ENOUGH_DATA) {
-            snapshot();
+    this.createRendererAndScene();
+}
 
-            var markers = detector.detect(imageData);
-            drawCorners(markers);
-            updateScene(markers);
+ARBase.prototype.createRendererAndScene = function () {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(40, canvas.width / canvas.height, 1, 1000);
 
-            render();
-        }
-    }
+    this.renderer = new THREE.WebGLRenderer({
+        antialias	: true,
+        alpha		: true
+    });
 
-    function init() {
-        detector = new AR.Detector();
-        detector.detect = function (image) {
-            var qr_data;
+    this.renderer.setSize(this.video.width, this.video.height);
+    document.body.appendChild(this.renderer.domElement);
 
-            CV.grayscale(image, this.grey);
-            CV.adaptiveThreshold(this.grey, this.thres, 2, 7);
+    var geometry = new THREE.BoxGeometry(1, 1, 1),
+        material = new THREE.MeshLambertMaterial({color: 0x009900}),
+        loader;
 
-            this.contours = CV.findContours(this.thres, this.binary);
+    THREE.CrossDomainJSONLoader = function (showStatus) {
+        THREE.JSONLoader.call(this, showStatus);
+    };
 
-            this.candidates = this.findCandidates(this.contours, image.width * 0.20, 0.05, 10);
-            this.candidates = this.clockwiseCorners(this.candidates);
-            this.candidates = this.notTooNear(this.candidates, 5);
+    loader = new THREE.JSONLoader();
 
-            qr_data = find_QR(this.candidates);
-            if (qr_data) {
-                return [new AR.Marker(2, qr_data)];
-            } else {
-                return [];
-            }
-        };
+    //animated_mesh;
+    var self = this;
+    loader.load('resources/models/figure/knight.js', function (geometry, materials) {
+        var standard = new THREE.MeshLambertMaterial({color: 0x9999ff}),
+            mats,
+            mesh,
+            scale;
 
-        posit = new POS.Posit(modelSize, canvas.width);
+        geometry.computeFaceNormals();
+        geometry.computeVertexNormals();
 
-        createRendererAndScene();
+        mats = new THREE.MeshFaceMaterial(materials);
 
-        window.requestAnimationFrame(tick);
-    }
+        mesh = new THREE.SkinnedMesh(geometry, mats);
+        mesh.visible = false;
 
-    function onLoad() {
-        canvas = document.getElementById('canvas');
-        video = document.getElementById('video');
-        context = canvas.getContext('2d');
+        self.scene.add(mesh);
+        
+        // animation
+        self.animation_mixer = new THREE.AnimationMixer( self.scene );
+        self.animation_clip = self.animation_mixer.clipAction( geometry.animations[0] );
+        self.animation_clip.play();
 
-        if (!window.URL) {
-            window.URL = window.URL || window.webkitURL || window.msURL || window.oURL;
-        }
+        self.animated_mesh = mesh;
+    });
 
-        if (!navigator.getUserMedia) {
-            navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-                    navigator.mozGetUserMedia || navigator.msGetUserMedia;
-        }
-
-        window.navigator.getUserMedia({
-            video: true,
-            audio: false
-        }, function (stream) {
-            try {
-                video.src = window.URL.createObjectURL(stream);
-                init();
-            } catch (err) {
-                video.src = stream;
-            }
-        }, function () {
-            throw new Error('Cannot capture user camera.');
+    { // box
+        var mat = new THREE.MeshBasicMaterial({
+            color : "#00FF00",
+            transparent: true,
+            opacity: 0.5
         });
+
+        var geom = new THREE.BoxGeometry(10, 10, 10);
+        var mesh = new THREE.Mesh(geom, mat);
+        this.testMesh = mesh;
+        this.scene.add( mesh );
     }
 
-    window.onload = onLoad;
-}();
+    this.camera.position.z = 5;
+
+    { // light
+        var light = new THREE.DirectionalLight(0xffffff, 1, 100000);
+        light.position.set(0, 0, 1);
+        this.scene.add(light);
+    }
+}
+
+ARBase.prototype.onTick = function (videoIsReady) {
+    if (videoIsReady) {
+        var imageData = this.snapShot();
+
+        var markers = this.detector.detect(imageData);
+        this.drawCorners(markers);
+        this.updateScene(markers);
+        this.render();
+    }
+}
+
+ARBase.prototype.drawCorners = function (markers) {
+    var corners, corner, i, j;
+
+    this.context.lineWidth = 3;
+
+    for (i = 0; i < markers.length; i += 1) {
+        corners = markers[i].corners;
+
+        this.context.strokeStyle = "red";
+        this.context.beginPath();
+
+        for (j = 0; j < corners.length; j += 1) {
+            corner = corners[j];
+            this.context.moveTo(corner.x, corner.y);
+            corner = corners[(j + 1) % corners.length];
+            this.context.lineTo(corner.x, corner.y);
+        }
+
+        this.context.stroke();
+        this.context.closePath();
+
+        this.context.strokeStyle = "green";
+        this.context.strokeRect(corners[0].x - 2, corners[0].y - 2, 4, 4);
+    }
+}
+
+ARBase.prototype.updateScene = function (markers) {
+    var corners, corner, pose, i;
+    var delta = 0.75 * this.clock.getDelta();
+
+    if (markers.length > 0) {
+        this.animated_mesh.visible = true;
+        this.testMesh.visible = true;
+
+        corners = markers[0].corners;
+
+        for (i = 0; i < corners.length; i += 1) {
+            corner = corners[i];
+            corner.x = corner.x - (this.canvas.width / 2);
+            corner.y = (this.canvas.height / 2) - corner.y;
+        }
+
+        pose = this.positEstimator.pose(corners);
+
+        this.updateObject(this.animated_mesh, pose.bestRotation, pose.bestTranslation, 3.0);
+        this.updateObject(this.testMesh, pose.bestRotation, pose.bestTranslation, 1.0);
+
+        this.animation_mixer.update( delta );
+    } else {
+        if (this.animated_mesh) {
+            this.animated_mesh.visible = false;
+        }
+        if (this.testMesh) {
+            this.testMesh.visible = false;
+        }
+    }
+}
+
+ARBase.prototype.updateObject = function (object, rotation, translation, scale) {
+    object.scale.set(scale, scale, scale);
+
+    object.rotation.x = -Math.asin(-rotation[1][2]);
+    object.rotation.y = -Math.atan2(rotation[0][2], rotation[2][2]);
+    object.rotation.z =  Math.atan2(rotation[1][0], rotation[1][1]);
+
+    object.position.x = translation[0];
+    object.position.y = translation[1];
+    object.position.z = -translation[2];
+}
+
+ARBase.prototype.render = function () {
+    this.renderer.render(this.scene, this.camera);
+}
+
+ARBase.prototype.animate = function (skinnedMesh) {
+    var materials = skinnedMesh.material.materials,
+        k;
+
+    for (k in materials) {
+        materials[k].skinning = true;
+    }
+
+    this.animation = new THREE.Animation(skinnedMesh, skinnedMesh.geometry.animation);
+    this.animation.play();
+}
+
+////////////////////////////////////////////////
+
+window.onload = function () {
+    mainVideo = new ARBase(window, 'canvas', 'video');
+    mainVideo.start();
+};
--- scripts/main_video.js
+++ scripts/main_video.js
@@ -1,95 +1,62 @@
-var mainVideo = function () {
-    'use strict';
-    var video, canvas, context, imageData, detector, posit,
-        element;
+var mainVideo = null;
 
-    function snapshot() {
-        context.drawImage(video, 0, 0, canvas.width, canvas.height);
-        imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-    }
 
-    function tick() {
-        window.requestAnimationFrame(tick);
+ARBase.prototype.onInit = function () {
+    this.element = document.getElementById('pinned-corners-video');
 
-        if (video.readyState === video.HAVE_ENOUGH_DATA) {
-            snapshot();
+    this.detector = new AR.Detector();
+    this.detector.detect = function (image) {
+        CV.grayscale(image, this.grey);
+        CV.adaptiveThreshold(this.grey, this.thres, 2, 7);
 
-            var markers = detector.detect(imageData);
-            /////////////////
-            if (markers.length > 0) { //if we detected marker
-                var idx = [0, 1, 3, 2], //correct order of marker vertices for pinned corners function
-                    corners = [],
-            //we arrange marker corners in idx order and map from video to window coordinates
-                    rect = video.getBoundingClientRect(),
-                    i;
+        this.contours = CV.findContours(this.thres, this.binary);
 
-                for (i = 0; i < 4; i += 1) {
-                    corners.push(markers[0].corners[idx[i]]);
-                    //corners[i].x = rect.left + corners[i].x;
-                    //corners[i].y = rect.top + corners[i].y;
-                }
+        this.candidates = this.findCandidates(this.contours, image.width * 0.20, 0.05, 10);
+        this.candidates = this.clockwiseCorners(this.candidates);
+        this.candidates = this.notTooNear(this.candidates, 5);
 
-                pinСorners(element, corners[0].x, corners[0].y, corners[1].x, corners[1].y, corners[2].x, corners[2].y, corners[3].x, corners[3].y);
-                element.style.visibility = "visible";
-            } else {
-                element.style.visibility = "hidden";
+        var qr_data = find_QR(this.candidates);
+        if (qr_data) {
+            return [new AR.Marker(2, qr_data)];
+        } else {
+            return [];
+        }
+    };
+};
+
+ARBase.prototype.onTick = function (videoIsReady) {
+    if (videoIsReady) {
+        var imageData = this.snapShot();
+
+        var markers = this.detector.detect(imageData);
+
+        if (markers.length > 0) { //if we detected marker
+            var idx = [0, 1, 3, 2], //correct order of marker vertices for pinned corners function
+                corners = [],
+        //we arrange marker corners in idx order and map from video to window coordinates
+                rect = this.video.getBoundingClientRect(),
+                i;
+
+            for (i = 0; i < 4; i += 1) {
+                corners.push(markers[0].corners[idx[i]]);
             }
+
+            pinСorners(
+                this.element,
+                corners[0].x, corners[0].y,
+                corners[1].x, corners[1].y,
+                corners[2].x, corners[2].y,
+                corners[3].x, corners[3].y);
+            this.element.style.visibility = "visible";
+        } else {
+            this.element.style.visibility = "hidden";
         }
     }
+};
 
-    function init() {
-        detector = new AR.Detector();
-        detector.detect = function (image) {
-            CV.grayscale(image, this.grey);
-            CV.adaptiveThreshold(this.grey, this.thres, 2, 7);
+////////////////////////////////////////////////
 
-            this.contours = CV.findContours(this.thres, this.binary);
-
-            this.candidates = this.findCandidates(this.contours, image.width * 0.20, 0.05, 10);
-            this.candidates = this.clockwiseCorners(this.candidates);
-            this.candidates = this.notTooNear(this.candidates, 5);
-
-            var qr_data = find_QR(this.candidates);
-            if (qr_data) {
-                return [new AR.Marker(2, qr_data)];
-            } else {
-                return [];
-            }
-        };
-        //posit = new POS.Posit(modelSize, canvas.width);
-        window.requestAnimationFrame(tick);
-    }
-
-    function onLoad() {
-        canvas = document.getElementById('canvas');
-        video = document.getElementById('video');
-        context = canvas.getContext('2d');
-        element = document.getElementById('pinned-corners-video');
-
-
-        if (!window.URL) {
-            window.URL = window.URL || window.webkitURL || window.msURL || window.oURL;
-        }
-
-        if (!navigator.getUserMedia) {
-            navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-                    navigator.mozGetUserMedia || navigator.msGetUserMedia;
-        }
-
-        window.navigator.getUserMedia({
-            video: true,
-            audio: false
-        }, function (stream) {
-            try {
-                video.src = window.URL.createObjectURL(stream);
-                init();
-            } catch (err) {
-                video.src = stream;
-            }
-        }, function () {
-            throw new Error('Cannot capture user camera.');
-        });
-    }
-
-    window.onload = onLoad;
-}();
+window.onload = function () {
+    mainVideo = new ARBase(window, 'canvas', 'video');
+    mainVideo.start();
+};
